53d056a9ad93a4987f230a4b9591719267907213
Orlando Zúniga <139894785+orlando-zh@users.noreply.github.com>
2024-09-11 12:13:09 -0600

    subiendo practica 3

---
+++ PRATICA3/Practica3APP/Forms/StudentSystem.cs
@@ -0,0 +1,36 @@
+﻿using StudentSystemData;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Practica3APP.Forms
+{
+    public partial class StudentSystem : Form
+    {
+        public StudentSystem()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Career career = new Career();
+            Student student = new Student();
+
+            career.Name = txtCarreraAlumno.Text;
+            student.Firstname = txt
+        }
+    }
+}
---
+++ PRATICA3/StudentSystemData/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSystemData
+{
+    public class Student
+    {
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public Career Career { get; set; }
+    }
+}
