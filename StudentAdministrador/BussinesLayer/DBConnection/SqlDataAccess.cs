959b77ca282668ec8902a77672c5db012fd677c2
orlando-zh <orlandozuniga4@gmail.com>
2024-09-18 13:53:01 -0600

    subiendo practica 6

---
+++ StudentAdministrador/BussinesLayer/DBConnection/SqlDataAccess.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayey.DBConnection
+{
+    public class SqlDataAccess
+    {
+        private readonly string _connetionString;
+
+        public SqlDataAccess()
+        {
+            _connetionString = "Data Source=LAPTOP-KFDQNCRJ\\SQLEXPRESS;Initial Catalog=StudentAdministratorDB;Integrated Security=True;Encrypt=False";
+        }
+
+        public SqlConnection GetSqlConnetion()
+        {
+            return new SqlConnection( _connetionString );
+        }
+    }
+}
---
+++ StudentAdministrador/CommonLayer/Entities/Career.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLayer.Entities
+{
+    public class Career
+    {
+        public int IdCareer { get; set; }
+        public string nameCareer { get; set; }
+
+        public string descriptionCareer { get; set; }
+    }
+}
